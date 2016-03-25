85f0650f865aa8dff79934cdf03e5d5983167593
fish47 <fish47@126.com>
2016-03-25 20:33:05 +0800

    123

--- tool/mergefiles.lua
+++ tool/mergefiles.lua
@@ -1,91 +1,102 @@
-local _CONST_STR_INDENT                 = "    "
-local _CONST_STR_EMPTY                  = ""
-local _CONST_READ_MODE_LINE_WITH_EOL    = "*L"
+local _STR_DIST_FILE_NAME               = "mpvdanmakuloader.lua"
+local _STR_READ_MODE_LINE_WITH_EOL      = "*L"
+local _STR_MODULE_FUNC_NAME_PATTERN     = "/%."
+local _STR_MODULE_FUNC_NAME_REPLACE     = "_"
 
-local _CONST_INDENT_MODULE_FILE_BLOCK   = 1
-local _CONST_INDENT_MODULE_FILE_CONTENT = 4
-
-local _CONST_STR_INNER_REQUIRE_PATTERN  = "/"
-local _CONST_STR_INNER_REQUIRE_REPLACE  = "_"
-
-local _PATTERN_LINE_WITH_EOL            = "(.-\r?\n)"
-
-
-local _STR_REQUIRE_HACK_START   = [[
+local _STR_MERGE_FILES_START    = [[
 local require = nil
 require = function(path)
 
 ]]
 
-local _STR_REQUIRE_HACK_END     = [[
+local _STR_MODULE_CONTENT_START = [[
+    if path == "%s"
+    then
+        local module = package.loaded[path]
+        if not module
+        then
+            local function %s()
+
+]]
+
+local _STR_MODULE_CONTENT_END   = [[
+
+            end
+            module = %s()
+        end
+        return module
+    end
+]]
+
+local _STR_MERGE_FILES_END      = [[
 
     return _G.require(path)
 end
+
 ]]
 
 
-local _STR_MODULE_FILE_START    = [[
-if path == "%s"
-then
-    local module = package.loaded[path]
-    if not module
+local function __doWriteModule(outFile, path, isMainFile)
+    local moduleFile = io.open(path)
+    if not moduleFile
     then
-        local function %s()
-
-]]
-
-local _STR_MODULE_FILE_END      = [[
-
-        end
-        module = %s()
+        return
     end
-    return module
-end
-]]
 
-
-local function __getInnerRequireFunctionName(path)
-    return path:gsub(_CONST_STR_INNER_REQUIRE_PATTERN, _CONST_STR_INNER_REQUIRE_REPLACE)
-end
-
-
-local function __getIndentString(level)
-    local indent = _CONST_STR_EMPTY
-    for i = 1, level
-    do
-        indent = indent .. _CONST_STR_INDENT
+    local funcName = nil
+    if not isMainFile
+    then
+        funcName = path:gsub(_STR_MODULE_FUNC_NAME_PATTERN, _STR_MODULE_FUNC_NAME_REPLACE)
+        outFile:write(string.format(_STR_MODULE_CONTENT_START, path, funcName))
     end
-    return indent
-end
 
-
-local function __writeStringWithIndent(outFile, str, indentLevel)
-    local indent = __getIndentString(indentLevel)
-    for line in str:gmatch(_PATTERN_LINE)
-    do
-        outFile:write(indent)
-        outFile:write(line)
-    end
-end
-
-
-local function _writeModuleFileContent(outFile, filePath, indentLevel)
-    local indent = __getIndentString(indentLevel)
-    local moduleFile = io.open(filePath)
     while true
     do
-        local line = moduleFile:read(_CONST_READ_MODE_LINE_WITH_EOL)
+        local line = moduleFile:read(_STR_READ_MODE_LINE_WITH_EOL)
         if not line
         then
+            moduleFile:close()
             break
         end
 
-        outFile:write(indent)
         outFile:write(line)
     end
-    moduleFile:close()
+
+    if not isMainFile
+    then
+        outFile:write(string.format(_STR_MODULE_CONTENT_END, funcName))
+    end
 end
 
 
-local function _writeModule(outFile, filePath)
+local function _writeModule(outFile, path)
+    return __doWriteModule(outFile, path, false)
 end
+
+local function _writeMain(outFile, path)
+    return __doWriteModule(outFile, path, false)
+end
+
+local function main()
+--    local distFile = io.open(_STR_DIST_FILE_NAME)
+    local distFile = io.stdout
+    if not distFile
+    then
+        return
+    end
+
+    distFile:write(_STR_MERGE_FILES_START)
+    _writeModule(distFile, "src/base/_algo.lua")
+    _writeModule(distFile, "src/base/_conv.lua")
+--    _writeModule(distFile, "src/base/classlite.lua")
+--    _writeModule(distFile, "src/base/constants.lua")
+--    _writeModule(distFile, "src/base/serialize.lua")
+--    _writeModule(distFile, "src/base/types.lua")
+--    _writeModule(distFile, "src/base/unportable.lua")
+--    _writeModule(distFile, "src/base/utf8.lua")
+--    _writeModule(distFile, "src/base/utils.lua")
+    distFile:write(_STR_MERGE_FILES_END)
+    distFile:close()
+end
+
+main()
